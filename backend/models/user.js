1b4466ce9b921d4dda6dfaa558c9f116f04dd17b
Mauricio Urdaneta <mauriurdaneta20@gmail.com>
2026-02-09 21:13:29 -0500

    agregando todas las condiciones de los modelos con respecto a la clave

--- backend/models/user.js
+++ backend/models/user.js
@@ -26,6 +26,56 @@ email:{ // email debeia ser único y obligatorio
     trim:true,
     match :[/\S+@\S+\.\S+/,'El correo no es valido']
     //  patron email
-}},
+},
+//  contraseña requerida minimo 10 carácteres
+password:{
+    type:String,
+    required:true,
+    minLength:10,
+    select:false // no incluir la contraseña en las consultas por defecto
 
-)
+},
+    // rol del usuario, restringe algunos valores especificos
+role:{
+    type:String,
+    enum:['admin','coordinador','auxiliar'], // solo estos valores son válidos
+    default:'auxiliar' // rol por defecto
+},
+
+//  usuarios activos 
+active:{
+    type:Boolean,
+    default:true, // Nuevos usuarios empiezan activos
+
+},
+
+
+}, {
+    timestampps:true, // agrega campos createdAt y updatedAt automaticamente
+    versionKey:false // elimina el campo __v que mongoose agrega por defecto para el control de versiones de mongoose
+},)
+
+//  Midleware encripta la contrasela antes de guardar el usuario
+userSchema.eachPath('save',async function(next){
+    // si el password no ha sido modificado, continuar sin encriptar
+    if(this.isMOdified('password')) return next()
+    
+    try{
+        // generar slat con complejidad de 10 rondas
+        //  mayor numero de rondas = mas seguro pero mas lento
+        const salt = await bcrypt.genSalt(10);
+        
+        // Encripta el password con salt generado
+        this.password = await bcrypt.hash(this.password,salt)
+
+        next() // continuar con el proceso de guardado
+    } catch(error){
+        //  si hay un error de encriptacion pasar el error al siguiente 
+        next(error)
+    }
+})
+
+
+//  Crear y exportar el modulo de usuario
+
+module.exports= mongoose.model('User',userSchema)
